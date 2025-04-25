789a2abd695f27fe343c8f16f6d497dca26f578a
NSUBB <insubbeckley@gmail.com>
2025-04-25 16:00:12 -0400

    Edit macro metadata.

--- Init.py
+++
@@ -1,9 +0,0 @@
-# Init.py
-"""
-This file initializes the VarSet Update addon.
-"""
-
-# Importing necessary libraries or modules (if needed)
-import FreeCAD
-
-FreeCAD.Console.PrintMessage("VarSet Update addon loaded\n")
